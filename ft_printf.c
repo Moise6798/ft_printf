1c093fb5d815b9a1f36a16b8d82371c2c652c8d5
Moise6798 <otzenberger.nicolas@gmail.com>
2023-10-31 13:21:35 +0100

    ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,22 +6,35 @@
 /*   By: niotzenb <niotzenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 09:41:16 by niotzenb          #+#    #+#             */
-/*   Updated: 2023/10/31 11:29:30 by niotzenb         ###   ########.fr       */
+/*   Updated: 2023/10/31 13:05:05 by niotzenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_found_c(char )
+int	ft_found_c(char c, int i)
 {
-	if ()
+	write(1, &c, 1);
+	return (i);
 }
+
+int	ft_found_s(char *s, int i)
+{
+	while (*s)
+	{
+		write(1, &(*s), 1);
+		s++;
+		i++;
+	}
+	return (i);
+}
+
 int	ft_after_percent(const char *format, va_list args, int j, int i)
 {
 	if (format[i] == 'c')
-		write(1, "c", 1);
+		j = ft_found_c(va_arg(args, int), i);
 	if (format[i] == 's')
-		write(1, "s", 1);
+		j = ft_found_s(va_arg(args, char *), i);
 	if (format[i] == 'p')
 		write(1, "p", 1);
 	if (format[i] == 'd')
@@ -55,16 +68,17 @@ int	ft_printf(const char *format, ...)
 		else
 		{
 			write(1, &format[i], 1);
-			i++;
+			j++;
 		}
-		j++;
+		i++;
 	}
+	va_end(args);
 	return (0);
 }
 
 int	main(void)
 {
-	ft_printf("mon printf --> hello42, c: %c, s: %s, d: %d\n", "q", "toto", 42);
-	printf("le vrai printf --> hello42, c: %s, s: %s, d: %d\n", "q", "toto", 42);
+	ft_printf("mon printf --> hello42, c: %c, s: %s\n", 'q', "toto");
+	printf("le vrai printf --> hello42, c: %c, s: %s, d: %d\n", 'q', "toto", 42);
 	return (0);
 }
